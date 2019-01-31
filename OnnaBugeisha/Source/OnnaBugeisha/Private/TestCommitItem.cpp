ab677fd05ae9f1473449cba267e1747a9de1b740
Alex Skinner <up883772@myport.ac.uk>
2019-01-31 12:22:16 +0000

    Testing Commit
    Checking I can commit to Git.

---
+++ OnnaBugeisha/Source/OnnaBugeisha/Private/TestCommitItem.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestCommitItem.h"
+
+TestCommitItem::TestCommitItem()
+{
+}
+
+TestCommitItem::~TestCommitItem()
+{
+}
