b7a0a27a95db9a8ce6a4f6fbbe2020c5849f9f85
Lewis <up909980@myport.ac.uk>
2019-02-18 22:08:10 +0000

    Initial Commit of version2

---
+++ OnnaBugeisha/Source/OnnaBugeisha/Public/OB_PlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "OB_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONNABUGEISHA_API AOB_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	virtual void BeginPlay() override;
+
+private:
+
+};
