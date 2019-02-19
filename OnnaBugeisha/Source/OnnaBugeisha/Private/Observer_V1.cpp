df7144a243a71efd9582e72972116279271c0241
Alex Skinner <up883772@myport.ac.uk>
2019-02-19 15:21:38 +0000

    Observer Class Created/Jumping gravity tweaked
    Observer class created using World Settings class (may be incorrect for requirements).
    Jumping gravity set to 2.0 to making jumping feel less floaty.

---
+++ OnnaBugeisha/Source/OnnaBugeisha/Private/Observer_V1.cpp
@@ -0,0 +1,4 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Observer_V1.h"
+
---
+++ OnnaBugeisha/Source/OnnaBugeisha/Public/Observer_V1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/WorldSettings.h"
+#include "Observer_V1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONNABUGEISHA_API AObserver_V1 : public AWorldSettings
+{
+	GENERATED_BODY()
+	
+};
